2bdf354cc1ac712856c673e5b716a0f5997256ab
Ronalds Sovas <sovas@greenbox.lv>
2016-04-01 07:15:15 +0300

    initalized impact command

---
+++ app/Console/Commands/Impact.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Impact extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'impact';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run Impact Service';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment("init the impact");
+    }
+}
