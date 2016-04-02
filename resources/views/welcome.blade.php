08cfb3c84104031f16fa568923d935f7417bc61a
Ronalds Sovas <sovas@greenbox.lv>
2016-04-02 23:23:59 +0300

    remainig time fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -172,6 +172,21 @@
     <script type="text/javascript">
         @if (isset($deadline))
 
+            function getTimeRemaining(endtime){
+              var t = Date.parse(endtime) - Date.parse(new Date());
+              var seconds = Math.floor( (t/1000) % 60 );
+              var minutes = Math.floor( (t/1000/60) % 60 );
+              var hours = Math.floor( (t/(1000*60*60)) % 24 );
+              var days = Math.floor( t/(1000*60*60*24) );
+              return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+              };
+            }
+
             function initializeClock(id, endtime){
               var clock = document.getElementById(id);
               var timeinterval = setInterval(function(){
