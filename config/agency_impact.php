ac7905cf9821e09a9e86eb850eaa38423560488a
Ronalds Sovas <sovas@greenbox.lv>
2016-04-03 13:37:00 +0300

    init agency idea

---
+++ config/agency_impact.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'active' => env('IMPACT_AGENCY' , false),
+    'host' => env('IMPACT_AGENCY_HOST'),
+    'user' => env('IMPACT_AGENCY_USER'),
+    'password' => env('IMPACT_AGENCY_PASSWORD'),
+    'queue' => env('IMPACT_AGENCY_QUEUE')
+    
+];
