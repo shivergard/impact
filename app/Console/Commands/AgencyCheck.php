9d59d59318be1a380849784b05cf8ea8cca24d6d
Ronalds Sovas <sovas@greenbox.lv>
2016-04-03 15:56:37 +0300

    added agency details

--- app/Console/Commands/AgencyCheck.php
+++ app/Console/Commands/AgencyCheck.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 
 use App\Helpers\RabbitMq;
+use App\Helpers\Agency;
+
 use Config;
 
 class AgencyCheck extends Command
@@ -17,6 +19,8 @@ class AgencyCheck extends Command
     protected $signature = 'impact:agency';
 
     private $rabbit;
+
+    private $agency;
 
     /**
      * The console command description.
@@ -36,6 +40,8 @@ class AgencyCheck extends Command
 
         if (Config::get('agency_impact.active')){
             $this->rabbit = RabbitMq::makeConnection(Config::get('agency_impact'));
+
+            $this->agency = new Agency();
 
             if ($this->rabbit->getStatus()){
 
@@ -59,7 +65,42 @@ class AgencyCheck extends Command
     }
 
 
-    public function prepareMessageResponse($message){
+    public function prepareMessageResponse($message = false){
 
+        $return = false;
+
+        if ($message){
+            
+            $return = json_decode($message->body, true);
+
+            $responsed = false;
+
+            if (json_last_error() == JSON_ERROR_NONE && is_array($return)){
+
+                $processed = false;
+
+                if ($return['action'] == 'create_test'){
+                    $testResponse = $this->agency->prepareTest($return);
+                    if ($testResponse)
+                        $processed = $this->rabbit->response($testResponse);    
+                }
+                
+
+                if ($processed){
+                    $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']); 
+                }
+
+            }else{
+                $this->error($message->body);
+                $return = array('status' => 1);
+            }
+
+            $this->info(json_encode($return));  
+            
+        }
+
+
+
+        return json_encode($return);
     }
 }
--- app/Helpers/Agency.php
+++ app/Helpers/Agency.php
@@ -8,7 +8,7 @@ namespace App\Helpers;
 Agency :
 
     Agency will send to impact msg to request new task :
-    Message form {'ident' : 'candidate'  , 'sender' : 'agency'}
+    Message form {'ident' : 'candidate'  , 'action' : 'create_test', 'sender' : 'agency'} 
 
     Agency will create task and respond in queue with ending [{queue}_response]
     Message form : { 'ident'  : 'candidate' , 'test_url' , 'reviewer' : 'agency'}
@@ -19,8 +19,63 @@ Agency :
 
 **/
 
+use App\Applicants;
 
 class Agency {
 
+    const STARTED = 1;
+    const FINISHED = 2;
+    const EXPIRED = 3;
+
+    private function validateApplicant($applicantModel){
+        
+        $return = false;
+
+        if ($applicantModel->status == 1 || Carbon::now()->diffInHours($applicantModel->created_at) <= 48){
+            $return = true;
+        }
+
+        return $return;
+    }
+
+    private function prepareTest($request){
+
+        //check maybe we have this test
+
+        $valid = true;
+
+        $applicants = Applicants::where('git_url' , $request['ident']);
+
+        if ($applicants->count() > 0){
+            // validate applicant
+            $applicantModel = $applicants->first();
+
+            if (!$this->validateApplicant($applicantModel)){
+                $this->statusMessage(self::EXPIRED);
+                $valid = false;
+            }
+
+        }else{
+            $applicantModel = new Applicants();
+            $applicantModel->git_url = $request['ident'];
+            $applicantModel->creditals = str_random(10);
+
+            $applicantModel->save();
+        }
+
+        if (!$valid){
+            return false;
+        }
+
+        $testMessage = array(
+            'ident'  => $request['ident'] , 
+            'test_url' => Config::get('app.url').'/tst/'.$applicantModel->creditals , 
+            'reviewer' => $request['sender']
+        );
+
+        return $testMessage;
+
+
+    }
 
 }
